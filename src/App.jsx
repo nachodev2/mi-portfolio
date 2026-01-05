1d6b36be6ccfb19581475f721acebce312fcfc7a
Tomas Ruiz <ruiztms20@gmail.com>
2026-01-05 14:09:00 -0300

    Primer commit: Estructura gral + Navbar y esctructura.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Navbar from './navbar/Navbar.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-scroll'
+import { FaBars, FaTimes, FaAlignRight } from 'react-icons/fa'
+import './Navbar.css'
+
+const links = [
+    {id: 1, link: 'Inicio'},
+    {id: 2, link: 'Sobre Mí'},
+    {id: 3, link: 'Proyectos'},
+    {id: 5, link: 'Contacto'},
+]
+
+export default function Navbar() {
+
+const [nav, setNav] = useState(false);
+const handleNav = () => { 
+    setNav(!nav);
+    };
+
+  return (
+    <div className='navbar'>
+        <div className='nav-content'>
+            <div className='logo'> Mi Portfolio </div>
+            <ul className='nav-links'>
+                {links.map(({id, link}) => (
+                    <li key={id} className='nav-link'>
+                        <Link to={link} smooth duration={500}>
+                            {link}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </div>
+  )
+}
