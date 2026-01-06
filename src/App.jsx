ad71bf97d613376de13b216dde35f2723bebd59d
Tomas Ruiz <ruiztms20@gmail.com>
2026-01-06 14:14:53 -0300

    Quinto commit: Finalicé el navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Navbar from './navbar/Navbar.jsx'
+import Home from './home/Home.jsx'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Home />
     </>
   )
 }
